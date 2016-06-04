68eef167a6912341f6c74daf5cdd0f7172ce8513
lyudison <lyudison@gmail.com>
2016-06-03 22:22:56 -0500

    add bag of words but data set too large to be processed

--- preprocess.py
+++ preprocess.py
@@ -1,98 +1,144 @@
-# extract features from the yelp raw data set
+# extract features from the yelp raw data set reviews and users
 
-from random import shuffle
+import random
 import json
-import re
 import csv
-import cPickle
-import user_modified_json as uj
-from sklearn import datasets, linear_model
+import datetime
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+# import cPickle
+# import user_modified_json as uj
+# from sklearn import datasets, linear_model
 
-def preprocess():
+def main():
 
     # read the review data set
-    reviews = []
-    with open('yelp_academic_dataset_review.json') as review_json_file:
-        for line in review_json_file:
-            reviews.append(json.loads(line))
+    print('parsing review data...')
+    reviews = parse_json('./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json')
     
     # read the user data set
-    users = uj.user_modified_json()
+    print('parsing user data...')
+    users = parse_json('./yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json')
 
+    # NOTE: use count of votes as label first
     # read the votes_time_lr prediction model
-    with open('votes_time_lr.pkl', 'rb') as fid:
-        lr = cPickle.load(fid)
+    # with open('votes_time_lr.pkl', 'rb') as fid:
+    #     lr = cPickle.load(fid)
     
-    # read the taste words
-    taste_words = []
-    with open('taste_words.txt') as taste_words_file:
-        for line in taste_words_file:
-            taste_words.append(line[:-1]) # ignore the \r
+    # sample the data
+    print('sampling from '+str(len(reviews))+' data set...')
+    review_samples = sample(reviews, 100000)
 
-    # extract following features from reviews and users
-    #   label: the quality of the review
-    #   1. length of review: int
-    #   2. has mentioned taste?: boolean
-    #   3. has mentioned cost?: boolean
-    #   4. count of reviews of the publisher: int
-    #   5. count of votes of the publisher: int
-    #   6. is the publisher an elite?: boolean
-    #   7. count of compliment of the publisher: int
-    #   8. count of fans of the publisher: int
-    #   9. count of friends of the publisher: int
-    # note: the publisher means the user who publish the review
-    # note2: the label is placed at the 0 index
-    myRange =  range(0,500000) 
-    shuffle(myRange)
-    rand_smpl = [ reviews[i] for i in sorted(myRange[:100000]) ]
-    data_set = extract(rand_smpl, users, taste_words,lr)
+    # extract
+    print('extracting features...')
+    data_set = extract(review_samples, users)
+
+    # split into training and testing data set
+    print('splitting data set...')
+    portion = 0.6
+    train_len = int(len(data_set)*portion)
+    train_set = data_set[:train_len]
+    test_set = data_set[train_len:]
 
     # write the data set into csv file
-    with open('data_set.csv', 'w+') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',')
-        csv_writer.writerows(data_set)
-    
-    myRange =  range(0,500000) 
-    shuffle(myRange)
-    rand_smpl = [ reviews[i] for i in sorted(myRange[:100000]) ]
-    data_set = extract(rand_smpl, users, taste_words,lr)
+    print('writing training data set...')
+    write_to_csv('./data_set/train_set.csv', train_set)
+    print('writing testing data set...')
+    write_to_csv('./data_set/test_set.csv', test_set)
 
-    # write the data set into csv file
-    with open('data_set_test.csv', 'w+') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',')
-        csv_writer.writerows(data_set)
+def extract(reviews, users):
 
-def extract(reviews, users, taste_words, lr):
+    # convert to dictionary and store only useful info for fast reading
+    users = extract_users(users)
+
+    # get bag of words for all reviews
+    bag_of_words = get_bag_of_words([review['text'] for review in reviews])
+
+    # extract features
     data_set = []
-    for review in reviews:
-        user = findUser(review['user_id'], users)
-        if not user:
-            print 'did not find user:', review['user_id']
+    now = datetime.datetime.now()
+    for i in range(len(reviews)):
+
+        review = reviews[i]
+
+        # check user
+        if review['user_id'] not in users:
+            print('did not find user:', review['user_id'])
             continue
-        post_time=[int(x.group()) for x in re.finditer(r'\d+', review['date'])]
-        post_duration=1.0*(2016-post_time[0])+1.0*(5-post_time[1])/12+1.0*(1.0-post_time[2])/12/30
-        factor=lr.predict([post_duration])[0]   
-        review_quality=1.0*review['votes']['useful']/factor
-        if(review_quality>0):
-            data_set.append([review_quality, len(review['text']), hasTaste(review['text'], taste_words), hasCost(review['text']), user['review_count'], user['votes'], user['elite'], user['compliments'], user['fans'], user['friends']])
+
+        # find the user
+        user = users[review['user_id']]
+
+        # normalize: review_quality = votes/day
+        review_date = datetime.datetime.strptime(review['date'], '%Y-%m-%d')
+        delta = now - review_date
+        days = divmod(delta.total_seconds(), 24 * 60 * 60)[0]
+        review_quality = sum(review['votes'].values()) / days
+
+        # combine the features
+        data_set.append([review_quality,\
+                         user['review_count'],\
+                         user['average_stars'],\
+                         user['vote_funny_count'],\
+                         user['vote_useful_count'],\
+                         user['vote_cool_count'],\
+                         user['friend_count'],\
+                         user['elite_count'],\
+                         user['compliment_count'],\
+                         user['fan_count'],\
+                         len(review['text'])]
+                         + list(bag_of_words[i]))
+        
     return data_set
 
-def findUser(user_id, users):
-    if users.has_key(user_id):
-        return users[user_id]
-    return None
+# convert json to dictionary
+def extract_users(users):
+    users_dict = {}
+    for user in users:
+        user_dict = {}
+        # extract useful infos
+        user_dict['review_count'] = int(user['review_count'])
+        user_dict['average_stars'] = float(user['average_stars'])
+        user_dict['vote_funny_count'] = int(user['votes']['funny'])
+        user_dict['vote_useful_count'] = int(user['votes']['useful'])
+        user_dict['vote_cool_count'] = int(user['votes']['cool'])
+        user_dict['friend_count'] = int(len(user['friends']))
+        user_dict['elite_count'] = int(len(user['elite']))
+        user_dict['compliment_count'] = sum(user['compliments'].values())
+        user_dict['fan_count'] = int(user['fans'])
+        # put in the users dictionary
+        users_dict[user['user_id']] = user_dict
+    return users_dict
 
-def hasTaste(text, taste_words):
-    # convert to lower case
-    text = text.lower()
-    # find word in taste words list
-    for word in taste_words:
-        if text.find(word) != -1:
-            return True
-    return False
+def parse_json(filename):
+    data = []
+    with open(filename) as json_file:
+        for line in json_file:
+            data.append(json.loads(line))
+    return data
 
-def hasCost(text):
-    # check whether there is a number
-    return re.match(r'\d', text) != None
+def sample(data, num):
+    num = min(num, len(data))
+    data_indices = list(range(len(data)))
+    random.shuffle(data_indices)
+    return [data[i] for i in data_indices[:num]]
 
-preprocess()
+# refer to http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+def get_bag_of_words(texts):
+
+    # count the occurrence of words in all texts
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(texts)
+    X_train_counts = X_train_counts.toarray()
+
+    # transform from occurrence to frequency
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+    return X_train_tfidf.toarray()
+
+def write_to_csv(filename, data_set):
+    with open(filename, 'w+') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',')
+        csv_writer.writerows(data_set)
+
+main()
--- user_modified_json.py
+++ user_modified_json.py
@@ -3,9 +3,6 @@ import csv
 import sys,re
 
 def user_modified_json():
-    # Fixes ASCII error in Python csv writer module
-    # reload(sys)
-    # sys.setdefaultencoding('utf-8')
 
     # Load json files by lines into data
     data = []
@@ -25,39 +22,39 @@ def user_modified_json():
                 attribute_name[keys]=0
     print attribute_name
 
-#Process data into csv compatible and desired format
+    #Process data into csv compatible and desired format
     array={}
-    for rows in data:
-        row_data={}
-        for keys in attribute_name:
-            if not rows.has_key(keys):
-                row_data[keys]=0
+    for user in data:
+        my_user={}
+        for key in attribute_name:
+            if not user.has_key(key):
+                my_user[key]=0
             else:
-                if(keys==u'friends'):
-                    row_data[keys]=len(rows[keys])
-                if(keys==u'votes'):
-                    row_data[keys]=rows[keys][u'funny']
-                if(keys==u'compliments'):
-                    row_data[keys]=len(rows[keys])
-                if(keys==u'elite'):
-                    row_data[keys]=len(rows[keys])
-                if(keys=='yelping_since'):
-                    since=[int(x.group()) for x in re.finditer(r'\d+', rows[keys])]
+                if(key=='friends'):
+                    my_user[key]=len(user[key])
+                if(key=='votes'):
+                    my_user[key]=user[key][u'funny']
+                if(key=='compliments'):
+                    my_user[key]=len(user[key])
+                if(key=='elite'):
+                    my_user[key]=len(user[key])
+                if(key=='yelping_since'):
+                    since=[int(x.group()) for x in re.finditer(r'\d+', user[key])]
                     Yelp_time=1.0*(2016-since[0])+1.0*(5-since[1])/12                
-                    row_data[keys]=Yelp_time
-                if(keys==u'review_count'):
-                    row_data[keys]=rows[keys]
-                #if(keys==u'user_id'):
-                #    row_data[keys]=rows[keys]
-                if(keys==u'name'):
-                    row_data[keys]=rows[keys]
-                if(keys==u'type'):
-                    row_data[keys]=rows[keys]
-                if(keys==u'fans'):
-                    row_data[keys]=rows[keys]
-                if(keys==u'average_stars'):
-                    row_data[keys]=rows[keys]                          
-        array[rows[u'user_id']]=row_data
+                    my_user[key]=Yelp_time
+                if(key=='review_count'):
+                    my_user[key]=user[key]
+                #if(key==u'user_id'):
+                #    my_user[key]=user[key]
+                if(key=='name'):
+                    my_user[key]=user[key]
+                if(key=='type'):
+                    my_user[key]=user[key]
+                if(key=='fans'):
+                    my_user[key]=user[key]
+                if(key=='average_stars'):
+                    my_user[key]=user[key]                          
+        array[user['user_id']]=my_user
     return array
 '''
 with open('data.json', 'w') as outfile:
